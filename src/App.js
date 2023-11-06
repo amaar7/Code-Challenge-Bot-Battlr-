5122c34f50a93c0c560e3b5433cf38e5b186a420
amaar <amaar.ahmednur@student.moringaschool.com>
2023-11-06 10:08:55 +0300

    changed things and added app.js file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import BotCollection from './BotCollection';
+import YourBotArmy from './YourBotArmy';
+import SortBar from './SortBar'; 
+import BotSpecs from './BotSpecs'; 
 
 function App() {
+  const [bots, setBots] = useState([]);
+  const [yourBotArmy, setYourBotArmy] = useState([]);
+  const [selectedBot, setSelectedBot] = useState(null); 
+
+  // Fetched bots data from the server 
+  useEffect(() => {
+    fetch('http://localhost:8001/bots')
+      .then((response) => response.json())
+      .then((data) => setBots(data));
+  }, []);
+
+  const addToArmy = (bot) => {
+    if (!yourBotArmy.includes(bot)) {
+      setYourBotArmy([...yourBotArmy, bot]);
+    }
+  };
+
+  const releaseBot = (bot) => {
+    const updatedArmy = yourBotArmy.filter((b) => b.id !== bot.id);
+    setYourBotArmy(updatedArmy);
+  };
+
+  const dischargeBot = (bot) => {
+    // Implemented the logic to discharge a bot, including a DELETE request to the server
+    fetch(`http://localhost:8001/bots/${bot.id}`, {
+      method: 'DELETE',
+    })
+      .then(() => {
+        releaseBot(bot);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BotCollection
+        bots={bots}
+        onAddToArmy={addToArmy}
+      />
+      <YourBotArmy
+        yourBotArmy={yourBotArmy}
+        onRelease={releaseBot}
+        onDischarge={dischargeBot}
+      />
+      {selectedBot && (
+        <BotSpecs bot={selectedBot} onEnlist={addToArmy} />
+      )}
+      <SortBar />
     </div>
   );
 }
